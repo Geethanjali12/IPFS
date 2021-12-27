bd73d0c1d702cff5ed78051383d6771e7dd35562
Geethanjali12 <geethanjali@sparkouttech.com>
2021-12-27 15:25:39 +0530

    ipfs

--- app.js
+++ app.js
@@ -1,45 +1,91 @@
+// const {create} = require("ipfs-http-client");
+// const fs = require("fs");
+// const path = require("path");
+
+// const express = require("express");
+// const app = express();
+
+
+// const client = create('https://127.0.0.1:5002')
+
+// app.get('/', (req,res) => {
+//     res.render('home');
+// });
+
+// app.post('/upload', (req, res) => {
+//     const file = req.files.file;
+//     const fileName = req.body.fileName;
+//     const filePath = '/files';
+
+//     file.mv(filePath, async(err)=>{
+//         if(err) {
+//             console.log('error');
+//             return res.status(500).send(err);
+//         }
+//         const fileHash = await addFile(fileName, filePath);
+//         fs.unlink(filePath,"utf8", (err,data) => {
+//             if(err) console.log(err);
+//         });
+
+//         res.render('upload', { fileName, fileHash});
+//     });
+//   });
+
+// const add =  async(fileName,filePath) => {
+//     const file = fs.readFileSync(filePath);
+//     const fileAdded = await ipfs.add({path:fileName, content:file});
+//     const fileHash = fileAdded.hash;
+//     return fileHash;
+// }
+
+// add();
+
+// app.listen(3000,() =>{
+//     console.log('App running in 3000 port');
+// });
 const {create} = require("ipfs-http-client");
 const fs = require("fs");
-const path = require("path");
-
-const express = require("express");
-const app = express();
-
-
-const client = create('https://127.0.0.1:5002')
-
-app.get('/', (req,res) => {
-    res.render('home');
-});
-
-app.post('/upload', (req, res) => {
-    const file = req.files.file;
-    const fileName = req.body.fileName;
-    const filePath = '/files';
-
-    file.mv(filePath, async(err)=>{
-        if(err) {
-            console.log('error');
-            return res.status(500).send(err);
+async function ipfsClient(){
+    const ipfs = await create(
+        {
+            host:"ipfs.infura.io",
+            port:5001,
+            protocol:"https"
         }
-        const fileHash = await addFile(fileName, filePath);
-        fs.unlink(filePath,"utf8", (err,data) => {
-            if(err) console.log(err);
-        });
+    );
+    return ipfs;
 
-        res.render('upload', { fileName, fileHash});
-    });
-  });
-
-const add =  async(fileName,filePath) => {
-    const file = fs.readFileSync(filePath);
-    const fileAdded = await ipfs.add({path:fileName, content:file});
-    const fileHash = fileAdded.hash;
-    return fileHash;
 }
 
-add();
+async function saveText(){
+    let ipfs = await ipfsClient();
+    let result = await ipfs.add("hello");
+    console.log(result);
+}
+// saveText();
 
-app.listen(3000,() =>{
-    console.log('App running in 3000 port');
-});
+async function saveFile() {
+
+    let ipfs = await ipfsClient();
+
+    let data = fs.readFileSync("./package.json")
+    let options = {
+        warpWithDirectory: false,
+        progress: (prog) => console.log(`Saved :${prog}`)
+    }
+    let result = await ipfs.add(data, options);
+    console.log(result)
+}
+saveFile()
+
+async function getData(hash) {
+    let ipfs = await ipfsClient();
+
+    let asyncitr = ipfs.cat(hash)
+
+    for await (const itr of asyncitr) {
+
+        let data = Buffer.from(itr).toString()
+        console.log(data)
+    }
+}
